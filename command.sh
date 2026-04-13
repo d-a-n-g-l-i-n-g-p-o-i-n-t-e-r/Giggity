01978c542a46d132e56b81646f2b216649c51563
d-a-n-g-l-i-n-g-p-o-i-n-t-e-r <nrbahytermek@gmail.com>
2026-04-13 15:51:12 -0400

    Update command.sh with arguments

--- command.sh
+++ command.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-docker run -v ./my_solution.py:/app/solution.py \
+read -p "Enter file to run or leave empty:" file
+file="${file:-my_solution.py}"
+
+echo ./"$file":/app/solution.py 
+docker run -v ./"$file":/app/solution.py \
 c0rp/innoforce.kz:sec-guard-latest --hook /app/solution.py
